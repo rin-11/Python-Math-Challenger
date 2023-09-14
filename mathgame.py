9749f5a067b654b3dab2b37917b3ecbec98055a0
Rin Kates <erinkates11@gmail.com>
2023-09-14 19:35:56 -0400

    set up

---
+++ mathgame.py
@@ -0,0 +1,9 @@
+# GOAL: randomnly generate math questions for user to calculate
+        # user cannot move on until the answer is correct
+        # set timer
+
+import random
+
+OPERATORS = ["+", "-", "/", "*"]
+MIN_OPERAND = 3
+MAX_OPERAND = 12
