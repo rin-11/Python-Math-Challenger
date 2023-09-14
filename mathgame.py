53246075fc23a69f1547102cc97b036c41f2a233
Rin Kates <erinkates11@gmail.com>
2023-09-14 19:50:31 -0400

    finished

--- mathgame.py
+++ mathgame.py
@@ -3,7 +3,40 @@
         # set timer
 
 import random
+import time
 
-OPERATORS = ["+", "-", "/", "*"]
+#variables
+OPERATORS = ["+", "-",  "*"]
 MIN_OPERAND = 3
 MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
+
+def generate_problem():
+    #select random operators and random intergers
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operator = random.choice(OPERATORS)
+    #create expression string
+    expr = str(left) + " " + operator + " " + str(right)
+    answer = eval(expr)
+    return expr, answer
+
+wrong = 0 #variable to keep track of the number of incorrect answers
+input("Press enter to start!")
+print("----------------------")
+
+start_time = time.time()
+
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    while True:
+        guess = input("Problem #" + str(i + 1) + ": " + expr + " = ")
+        if guess == str(answer): #check exptression
+            break
+        wrong += 1
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+
+print("----------------------")
+print("FINISHED! ALL 10 PROBLEMS WERE ANSWERED CORRECTLY IN", total_time, "SECONDS.")
